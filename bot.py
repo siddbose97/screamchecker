ec02a4dfbe1ab1723c74e6d071b8b57efd7aa5ae
Siddhartha Bose <siddbose97@gmail.com>
2022-06-03 15:09:33 +0400

    increasing wait

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
 import os
 import time
 from selenium.webdriver.common.by import By
@@ -39,35 +40,37 @@ browser = webdriver.Chrome(service=Service(os.getenv('CHROME_EXECUTABLE_PATH')),
 is_liquidity = False
 counter  = 1
 while not is_liquidity:
-    #driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()), options = firefox_options)
-    driver = webdriver.Chrome(options = option)
-    print("in while loop")
-    # try:
-    URL = 'https://v1.scream.sh/lend'
-    driver.get(URL)
-    print("proceeding to sleep")
+    try:
+        #driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()), options = firefox_options)
+        driver = webdriver.Chrome(options = option)
+        print("in while loop")
+        # try:
+        URL = 'https://v1.scream.sh/lend'
+        driver.get(URL)
+        print("proceeding to sleep")
 
-    #time.sleep(40)
-    wait = WebDriverWait(driver, 100)
-    block = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[4]/div/div[3]/div[2]/table/tbody/tr[18]/td[4]/div')))
-    #liquidity_usdc = driver.find_elements_by_xpath('/html/body/div[1]/div[4]/div/div[3]/div[2]/table/tbody/tr[18]/td[4]/div')
-    liquidity_usdc = driver.find_element(By.XPATH, '/html/body/div[1]/div[4]/div/div[3]/div[2]/table/tbody/tr[18]/td[4]/div')
+        #time.sleep(40)
+        wait = WebDriverWait(driver, 100)
+        block = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[4]/div/div[3]/div[2]/table/tbody/tr[18]/td[4]/div')))
+        #liquidity_usdc = driver.find_elements_by_xpath('/html/body/div[1]/div[4]/div/div[3]/div[2]/table/tbody/tr[18]/td[4]/div')
+        liquidity_usdc = driver.find_element(By.XPATH, '/html/body/div[1]/div[4]/div/div[3]/div[2]/table/tbody/tr[18]/td[4]/div')
 
-    if float(liquidity_usdc.text) > 10:
-        #send_email(f"liquidity is currently {liquidity_usdc.text}")
-        send_telegram_message(f"liquidity is currently {liquidity_usdc.text}", chat_id, api_key)
-        is_liquidity = True
-    
-    if counter == 15:
-        #send_email(f"15 min update, liquidity is currently {liquidity_usdc.text}")
-        send_telegram_message(f"15 min update, liquidity is currently {liquidity_usdc.text}", chat_id, api_key)
-        counter = 1
+        if float(liquidity_usdc.text) > 10:
+            #send_email(f"liquidity is currently {liquidity_usdc.text}")
+            send_telegram_message(f"liquidity is currently {liquidity_usdc.text}", chat_id, api_key)
+            is_liquidity = True
+        
+        if counter == 15:
+            #send_email(f"15 min update, liquidity is currently {liquidity_usdc.text}")
+            send_telegram_message(f"15 min update, liquidity is currently {liquidity_usdc.text}", chat_id, api_key)
+            counter = 1
 
-    print(liquidity_usdc.text)
-    time.sleep(30)
-    driver.quit()
-    counter += 1
-
+        print(liquidity_usdc.text)
+        time.sleep(30)
+        driver.quit()
+        counter += 1
+    except TimeoutException:
+        print("timedout")
     # except:
     #     is_liquidity = True
     #     driver.quit()
